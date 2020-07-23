a059e91a370def7963055fb2699a0ea7b04c05fe
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-23 18:57:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   original_array = sentence.split("")
   new_array = []
   original_array.each do |sentence|
-    new_array << string.reverse
+    new_array << sentence.reverse
   end
   new_array.join("")
 end
